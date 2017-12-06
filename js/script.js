c0916b44159d73e2ac314adf0d9745ff07ac4ed4
ElenaO <post032@yandex.ru>
2017-12-06 17:05:51 +0300

    проверка

---
+++ js/script.js
@@ -0,0 +1,7 @@
+var button = document.querySelector(".button");
+var modal = document.querySelector(".modal");
+
+button.addEventListener("click", function (evt) {
+  evt.preventDefault();
+  modal.classList.toggle("modal-close");
+});
